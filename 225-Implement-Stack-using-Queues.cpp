f9a9035db24751864516fee1209fd937bc374891
Omar Khalid <137959170+omarovici@users.noreply.github.com>
2025-02-16 01:15:42 +0200

    [225] [Time Beats: 100.00%] [Memory Beats: 20.04%] - LeetPush

--- 225-Implement-Stack-using-Queues.cpp
+++ 225-Implement-Stack-using-Queues.cpp
@@ -41,13 +41,3 @@ class MyStack {
         return q.empty();
     }
 };
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
-// @lc code=end
