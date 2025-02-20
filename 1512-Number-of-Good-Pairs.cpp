01ec08f1a1611fc8761f2360a3cb3c1c172cef3b
Omar Khalid <137959170+omarovici@users.noreply.github.com>
2025-02-20 20:48:15 +0200

    [1512] [Time Beats: 100.00%] [Memory Beats: 80.80%] - LeetPush

--- 1512-Number-of-Good-Pairs.cpp
+++ 1512-Number-of-Good-Pairs.cpp
@@ -4,7 +4,7 @@ public:
         int cnt = 0 ;
         for (int i = 0 ; i < nums.size() ; i++){
             for (int j = i + 1 ; j < nums.size() ; j++){
-                if (nums[i] == nums[j]){
+                if (nums[i] == nums[j] && i<j){
                     cnt++;
                 }
             }
